a643f0897809447d8b2694394e70327681a90ff9
winpainghein00 <winpainghein75@gmail.com>
2024-11-05 20:48:37 +0630

    background update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Input from './components/Input'
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-custom-img bg-cover bg-center bg-no-repeat flex justify-center items-center'>
+    // <div className='min-h-screen bg-custom-img bg-cover bg-center bg-no-repeat flex justify-center items-center'>
+    <div className='min-h-screen bg-gradient-to-r from-green-800 to-black flex justify-center items-center'>
       <UiCard/>
       <Toaster position='top-right'/>
     </div>
