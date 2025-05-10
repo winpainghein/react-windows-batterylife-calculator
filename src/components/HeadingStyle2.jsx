9dfc6f874b7b5f39fc1033cda66a50a7ba7835c6
winpainghein <winpainghein75@gmail.com>
2025-05-10 11:46:10 +0630

    Fix Open Graph and Twitter URLs; update heading description for clarity

--- src/components/HeadingStyle2.jsx
+++ src/components/HeadingStyle2.jsx
@@ -4,8 +4,8 @@ export default function HeadingStyle2() {
       <h1 className="md:text-4xl text-2xl font-bold text-gray-300 dark:text-white">
         WPH Battery Health Calculator
       </h1>
-      <p className="md:mt-1.5 mt-1 text-sm md:w-auto w-[80%] mx-auto text-gray-400 dark:text-gray-300">
-        Estimate your Windows laptop battery health using mWh data.
+      <p className="md:mt-1.5 mt-1 text-sm md:w-auto w-[75%] mx-auto text-gray-400 dark:text-gray-300">
+        Estimate your Windows laptop battery health percentage using mWh data.
       </p>
     </div>
   );
