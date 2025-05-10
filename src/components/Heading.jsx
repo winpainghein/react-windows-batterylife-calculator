39eb7a80c14f3528f695122d7ffc774f474f258a
winpainghein <winpainghein75@gmail.com>
2025-05-10 11:27:37 +0630

    Update HTML metadata and favicon; correct heading text for clarity

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ const Heading = () => {
   return (
      <header className="bg-white/30 backdrop-blur-md border border-white/10 rounded-lg shadow-lg max-w-lg mx-auto mb-8 p-6 text-center">
       <h1 className="text-2xl sm:text-3xl font-bold text-gray-800 dark:text-white">
-        WPH BatteryLife Calculator
+        WPH Battery Health Calculator
       </h1>
       <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
         Estimate your Windows laptop battery health using mWh data.
