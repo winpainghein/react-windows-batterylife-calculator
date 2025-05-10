eb2408bde21c7d572981dd5cc239e6fcf36c74f6
winpainghein <winpainghein75@gmail.com>
2025-05-10 12:43:19 +0630

    Update min-height class for improved layout responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import HeadingStyle2 from './components/HeadingStyle2'
 const App = () => {
   return (
     // <div className='min-h-screen bg-custom-img bg-cover bg-center bg-no-repeat flex justify-center items-center'>
-    <div className='min-h-screen bg-gradient-to-r from-green-800 to-black flex flex-col justify-center items-center'>
+    <div className='md:min-h-screen min-h-dvh bg-gradient-to-r from-green-800 to-black flex flex-col justify-center items-center'>
       <HeadingStyle2/>
       <UiCard/>
       <Note/>
